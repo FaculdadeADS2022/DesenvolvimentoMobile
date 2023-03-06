ac74b1a1c0dd0fbbf233cafc6da65bf50215d760
Heliton <102123948+HelitonCalgaroto@users.noreply.github.com>
2023-03-06 16:12:47 -0300

    Update index.js

--- calculoMedia/index.js
+++ calculoMedia/index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import CalculoMedia from './src/screens/CalculoMedia';
+import CalculoMedia from './src/screens/media';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => CalculoMedia);
